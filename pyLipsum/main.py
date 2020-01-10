45eab2037b27fb295f23918397cd3fca130f475b
Raúl Blanco <rbr007.movil@gmail.com>
2020-01-10 20:51:15 +0100

    Original Lorem ipsum, and better composition at the start
    - Created dictionary for Lorem ipsum.
    - Check if starting words are a phrase, if it has a period at the end.
      - If true, generate 1 phrase less.
      - If not, and start has more words than numWords (random), make it a
        phrase, and generate 1 less.
    - Better comments, block by block.

--- pyLipsum/main.py
+++ pyLipsum/main.py
@@ -1,28 +1,49 @@
-from chiquitoDict import chiquitoDict
 from random import choice, randint
 import logging
 
+# Import dictionaries
+from dicts.chiquitoDict import chiquitoDict
+from dicts.ipsumDict import ipsumDict
+
 logging.basicConfig(format='\n%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def ipsum(dict: classmethod, paragraphs: int, lorem: bool):
+def ipsum(dict: classmethod, numParags: int, lorem: bool):
+    ### Start
     output = "" # Variable declaration
-    if lorem: output = dict.start # If start is desired
-    for i in range(paragraphs): # Loop for paragraphs
+    if lorem:
+        output = start = dict.start.strip() # Trim spaces on start
+    ### Paragraph loop
+    for i in range(numParags):
         numPhrases = randint(5, 10)
-        for j in range(numPhrases): # Loop for phrases
+        ### Phrase loop
+        for j in range(numPhrases):
             numWords = randint(4, 12)
-            for k in range(numWords): # Loop for every array item selected
+            ### "Word" or array item loop
+            for k in range(numWords):
+                ### Check start
+                if i == j == 0 and lorem: # At first phrase
+                    if start[-1:] == ".":
+                        break # If start is a phrase, generate 1 less
+                    elif len(start.split()) > numWords:
+                        if output[-1:] != ".":
+                            output = output + "."
+                        break # If start is has no period, and has more words than numWords, make it a phrase
+                ### Random text
                 word = choice(dict.array) # Random array item
-                if k == 0 and not (i == j == 0 and lorem):
+                if k == 0 and not (i == j == 0 and lorem and not start[-1:] == "."):
                     word = word.capitalize() # Capitalize every start of phrase
-                if j == 0 and k == 0 and not (i == 0 and lorem):
+                if j == k == 0 and not (i == 0 and lorem):
                     output = output + word # Do not separate by space at start of paragraph
                 else:
                     output = output + " " + word
-            output = output + "." # Dot every end of phrase
-        if paragraphs > 1 and i < (paragraphs - 1):
+            ### Punctuation.
+            if output[-1:] != ".":
+                output = output + "." # Dot every end of phrase
+        ### Blank lines every paragraph
+        if numParags > 1 and i < (numParags - 1):
             output = output + "\n\n" # Separate paragraphs if more than one
+    ### Result
     print(output) # Print result
 
 if __name__ == '__main__':
-    ipsum(chiquitoDict, 3, True)
+    ipsum(ipsumDict, 2, True)
