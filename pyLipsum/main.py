943ca3dfea2b26df970e485ad089acc62b2c0b9a
Raúl Blanco <rbr007.movil@gmail.com>
2020-01-20 00:57:42 +0100

    Even better generation and optimization. Such a restructure.
    - Outputs every paragraph, instead of all at once, for better speed and
      optimization of memory.
    - Restructure of checking of generated items.
    - Checks number of words on start phrase, and generates number less of
      words on first random phrase. (fixes #1)
    - Checks if selected dictionary item is a phrase, and generates one
      random less, and punctuates output if not a start of paragraph.
      (fixes #2)
    - Checks if punctuation of dictionary item was going to generate more
      phrases than expected.
    - Checks number of words in array item, and generates number of words
      less in that phrase. (fixes #3)

--- pyLipsum/main.py
+++ pyLipsum/main.py
@@ -16,10 +16,12 @@ def ipsum(dict: classmethod, numParags: int, lorem: bool):
     for i in range(numParags):
         numPhrases = randint(5, 10)
         ### Phrase loop
-        for j in range(numPhrases):
+        j = 0
+        while j < numPhrases:
             numWords = randint(4, 12)
             ### "Word" or array item loop
-            for k in range(numWords):
+            k = 0
+            while k < numWords:
                 ### Check start
                 if i == j == 0 and lorem: # At first phrase
                     if start[-1:] == ".":
@@ -27,23 +29,37 @@ def ipsum(dict: classmethod, numParags: int, lorem: bool):
                     elif len(start.split()) > numWords:
                         if output[-1:] != ".":
                             output = output + "."
-                        break # If start is has no period, and has more words than numWords, make it a phrase
+                        break # If start has no period, and has more words than numWords, make it a phrase
+                    elif k == 0: # Check words on start, and generate number of words less on first random phrase
+                        k += len(start.split())
                 ### Random text
                 word = choice(dict.array) # Random array item
-                if k == 0 and not (i == j == 0 and lorem and not start[-1:] == "."):
+                if len(word.strip().split()) > 1: # If dictionary item has more than 1 word, generate number less
+                    k += (len(word.strip().split()) - 1)
+                if word.strip()[-1:] == ".": 
+                    k = numWords # If dictionary item is a phrase (has period), generate 1 phrase less
+                    if output.strip()[-1:] != "." and output != "":
+                        output = output + "." # Punctuate aready generated paragraph output if necessary
+                        j += 1 # Count phrase if there was more output generated
+                if output.strip()[-1:] == "." or output == "":
                     word = word.capitalize() # Capitalize every start of phrase
-                if j == k == 0 and not (i == 0 and lorem):
-                    output = output + word # Do not separate by space at start of paragraph
-                else:
-                    output = output + " " + word
+                if j < numPhrases: # Check if phrase on item array is generating more phrases than expected
+                    if output == "": # Space every word and start of phrase, not start of paragraph
+                        output = word
+                    else:
+                        output = output + " " + word
+                k += 1
             ### Punctuation.
-            if output[-1:] != ".":
+            if output.strip()[-1:] != ".":
                 output = output + "." # Dot every end of phrase
+            ###Phrase loop
+            j += 1
         ### Blank lines every paragraph
         if numParags > 1 and i < (numParags - 1):
-            output = output + "\n\n" # Separate paragraphs if more than one
-    ### Result
-    print(output) # Print result
+            output = output + "\n" # Separate paragraphs if more than one
+        ### Print paragraph and clear output variable
+        print(output)
+        output = ""
 
 if __name__ == '__main__':
-    ipsum(ipsumDict, 2, True)
+    ipsum(chiquitoDict, 5, True)
